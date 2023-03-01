992f8d17da0061d09ea4637b61099e164b55e6b2
enyllaoliveira <enyllaoliveira@gmail.com>
2023-03-01 08:35:54 -0300

    Inserção dos dados no carrinho por meio do localStorage

--- index010.js
+++ index010.js
@@ -1,5 +1,6 @@
  let cardGroup = document.getElementsByClassName('card-group')[0]
- 
+ let itensFormated = []
+
  let itensCart = [
     {
         titulo: 'Celular',
@@ -21,7 +22,7 @@
  function mountItens() {
     for(let i = 0; i < itensCart.length; i++) {
       let carde = document.createElement('div')
-    carde.classList.add('card')
+      carde.classList.add('card')
       cardGroup.appendChild(carde)
 
       let text = document.createElement('span')
@@ -41,12 +42,18 @@
 
       let btn = document.createElement('button')
       btn.addEventListener('click', function() { 
-      addItem(itensCart[i]) // está chamando a função / addStorageItem é o nome da função
+      addItem(itensCart[i]) // está chamando a função / addItem é o nome da função
       })
       btn.innerText = 'Adicionar'
       carde.appendChild(btn)    
     }
+
  }
- mountItens()
- function addItem(item) { // está criando a função - o "item" é pra relacionar com o AddItem
- } 
+ mountItens() // invocando a função 
+  function addItem(item) { // está criando a função - o "item" é pra relacionar com o AddItem
+  itensFormated.push(item)
+  
+  localStorage.setItem('carrinho', JSON.stringify(itensFormated));
+
+} 
+
