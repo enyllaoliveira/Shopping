9bd057e12fc3612414811211a28175f52952af99
enyllaoliveira <enyllaoliveira@gmail.com>
2023-07-05 18:15:12 -0300

    mudanças na estrutura

--- index010.js
+++ index010.js
@@ -3,18 +3,21 @@
 
  let itensCart = [
     {
+        id: 1,
         titulo: 'Celular',
         imagem: './assets/celular.png',
         preco: 500,
         quantidade: 1
     },
     {
+        id:2,
         titulo: 'Capinhas',
         imagem: './assets/capinhas.png',
         preco: 20,
         quantidade: 1
     },
     {
+        id:3,
         titulo: 'Fones',
         imagem: './assets/fones.png',
         preco: 30,
@@ -26,8 +29,7 @@
     for(let i = 0; i < itensCart.length; i++) {
       let carde = document.createElement('div')
       carde.classList.add('card')
-      cardGroup.appendChild(carde)
-
+     
       let text = document.createElement('span')
       text.innerText = itensCart[i].titulo
       text.classList.add('titulo')
@@ -49,18 +51,29 @@
       })
       btn.innerText = 'Adicionar'
       carde.appendChild(btn)    
+      cardGroup.appendChild(carde)
     }
-
  }
- mountItens() // invocando a função 
-  function addItem(item) { // está criando a função - o "item" é pra relacionar com o AddItem
-  if (localStorage.getItem('carrinho')) {
-    let acumuladorCar = JSON.parse(localStorage.getItem('carrinho'))
-    itensFormated = [...acumuladorCar, item]
-    localStorage.setItem('carrinho', JSON.stringify(itensFormated));
-    
-  } else {
-    itensFormated.push(item)
-    localStorage.setItem('carrinho', JSON.stringify(itensFormated));
+
+ mountItens() // invocando a função - está montando cada card e adicionando como o filho do cardGroup (linha 54)
+
+
+  function addItem(itemCart) { // está criando a função - o "item" é pra relacionar com o AddItem - o item é um parâmetro da funçao que fica disponível p todo escopo de AddItem
+    let itensClicados = [itemCart] 
+    if (localStorage.getItem('carrinho')) {
+      let getItensLocalStorage = JSON.parse(localStorage.getItem('carrinho'))
+      let existe = getItensLocalStorage.findIndex(item => item.id === itemCart.id)
+    if (existe != -1) {
+      getItensLocalStorage[existe].quantidade += 1
+    } else {
+      getItensLocalStorage = [...getItensLocalStorage, itemCart]
+    }
+      localStorage.setItem('carrinho', JSON.stringify(getItensLocalStorage))
+      /* VERIFICAR SE ITEM QUE ESTÁ SENDO CLICADO É IGUAL AO EXISTENTE NO CARRINHO*/
+      } else {
+      localStorage.setItem('carrinho', JSON.stringify(itensClicados)) 
+    }
   }
-}
+
+  
+
